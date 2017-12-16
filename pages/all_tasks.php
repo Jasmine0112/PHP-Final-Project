94b1fd0e3f0150f198362e5a5e2ea2f20e5037ae
Jasmine Morrison <Jasminemorrison12@yahoo.com>
2017-12-16 18:00:22 -0500

    Create all_tasks.php

---
+++ pages/all_tasks.php
@@ -0,0 +1,58 @@
+<?php
+
+use utility\htmlTags;
+
+class all_tasks extends utility\page
+
+{
+
+    protected $page;
+
+
+
+    public function buildPage($content = '')
+
+    {
+
+        return $this->page .= $content . '<br>';
+
+
+
+    }
+
+
+
+}
+
+
+
+$newPage = new all_tasks();
+
+
+
+$header = '<div class="container"><h1>All Tasks</h1></div>';
+
+
+
+$links = '<div class="container">
+
+<h3><a href="index.php?page=accounts&action=all">Show All Accounts</a></h3></div>';
+
+
+
+
+
+$newPage->buildPage($header);
+
+$newPage->buildPage($links);
+
+$table= utility\htmlTable::genarateTableFromMultiArray($data);
+
+$newPage->buildPage($table);
+
+$newPage->setHtml($newPage->buildPage());
+
+
+
+?>
+
