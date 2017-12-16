2b77d3589a9c33b6b81b76e5403c1e81283e80b8
Jasmine Morrison <Jasminemorrison12@yahoo.com>
2017-12-16 18:11:11 -0500

    updates to the return of build page

---
+++ pages/notfound.php
@@ -0,0 +1,57 @@
+<?php
+
+use utility\htmlTags;
+
+class show_task extends utility\page
+
+{
+
+    protected $page;
+
+
+
+    public function buildPage($content = '')
+
+    {
+
+        return $this->page .= $content . '<br>';
+
+
+
+    }
+
+
+
+}
+
+
+
+$newPage = new show_task();
+
+
+
+$header = '<div class="container"><center><h1>Oops! Something went wrong</h1></center></div>';
+
+
+
+$links = '<div class="container">
+
+<h3><a href="index.php?page=homepage">Go to Home</a></h3></div>';
+
+
+
+$errorPage = '<div class="container"><div class="panel text-center"><h1>HTTP Status 404: Page Not Found</h1></div></div>';
+
+
+
+$newPage->buildPage($header);
+
+$newPage->buildPage($links);
+
+$newPage->buildPage($errorPage);
+
+$newPage->setHtml($newPage->buildPage());
+
+
+
+?>
