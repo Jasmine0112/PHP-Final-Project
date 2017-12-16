788ebd7f62f50702978422bed76fc5833881a241
Jasmine Morrison <Jasminemorrison12@yahoo.com>
2017-12-16 17:57:57 -0500

    Create admin.php

---
+++ pages/admin.php
@@ -0,0 +1,95 @@
+<?php
+
+/*if($_SESSION['login'] == FALSE){
+
+    header('Location: index.php?page=accounts&action=login');
+
+}*/
+
+use utility\htmlTags;
+
+class admin extends utility\page
+
+{
+
+    protected $page;
+
+
+
+    public function buildPage($content = '')
+
+    {
+
+        return $this->page .= $content . '<br>';
+
+
+
+    }
+
+    public function getHeader()
+
+    {
+
+        $header ='';
+
+        if (isset($data)) {
+
+            $header .= '<div class="container jumbotron"><center><h3>' . $data . '</h3></center></div>';
+
+        } else {
+
+            $header .= ' <div class="container"><center><h1>Welcome to WebBull</h1></center>
+
+                <blockquote><b>Get more done with WebBull!</b><p>Create and Manage todo tasks<br>
+
+                Upload and view insights from CSV files<br>
+
+                Much more to come</p></blockquote>
+
+            </div>';
+
+
+
+        }
+
+        return $header;
+
+    }
+
+
+
+}
+
+
+
+$newPage = new admin();
+
+
+
+
+
+$links = '<div class="container">
+
+            <h3><a href="index.php?page=accounts&action=all">Show All Accounts</a></h3>
+
+            <h3><a href="index.php?page=tasks&action=all">Show All Tasks</a></h3>
+
+        </div>';
+
+
+
+$newPage->buildPage($newPage->getHeader());
+
+$newPage->buildPage($links);
+
+$newPage->buildPage($loginForm);
+
+
+
+$newPage->setHtml($newPage->buildPage());
+
+
+
+
+
+?>
