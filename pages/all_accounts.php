9657fd26c3b4e500f457f56ef744b58462a7a5ef
Jasmine Morrison <Jasminemorrison12@yahoo.com>
2017-12-16 17:59:24 -0500

    Changes  new page functions

---
+++ pages/all_accounts.php
@@ -0,0 +1,58 @@
+<?php
+
+use utility\htmlTags;
+
+class all_accounts extends utility\page
+
+{
+
+    protected $page;
+
+
+
+    public function buildPage($content = '')
+
+    {
+
+        return $this->page .= $content . '<br>';
+
+
+
+    }
+
+
+
+}
+
+
+
+$newPage = new all_accounts();
+
+
+
+$header = '<div class="container"><h1>User Accounts</h1></div>';
+
+
+
+$links = '<div class="container">
+
+<h3><a href="index.php?page=tasks&action=all">Show All Tasks</a></h3></div>';
+
+
+
+
+
+$newPage->buildPage($header);
+
+$newPage->buildPage($links);
+
+$table= utility\htmlTable::genarateTableFromMultiArray($data);
+
+$newPage->buildPage($table);
+
+$newPage->setHtml($newPage->buildPage());
+
+
+
+?>
+
